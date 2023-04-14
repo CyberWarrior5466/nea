11fe7b48987dc70816cd6d780a32bc8b532c4954
Ali Saleem <asaleen898@gmail.com>
2023-04-14 11:30:26 +0100

    minor changes

--- setup.sh
+++ setup.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # # ubuntu
-# sudo apt install hatch pandoc texlive-xetex fonts-firacode ttf-mscorefonts-installer npm -y 
+# sudo apt install hatch pandoc texlive-xetex fonts-firacode ttf-mscorefonts-installer npm fossil -y 
 
 # # fedora
 # sudo dnf install 'dnf-command(copr)'
 # sudo dnf copr enable adrienverge/some-nice-fonts -y
-# sudo dnf install hatch pandoc texlive-xetex fira-code-fonts some-nice-fonts npm librsvg2-tools texlive-scheme-medium -y
+# sudo dnf install hatch pandoc texlive-xetex fira-code-fonts some-nice-fonts npm librsvg2-tools texlive-scheme-medium fossil -y
 
 
-cd report
-mkdir mermaid
-cd mermaid
+# cd report
+# mkdir mermaid
+# cd mermaid
 # https://github.com/mermaid-js/mermaid-cli#install-locally
-sudo npm install -g @mermaid-js/mermaid-cli
+# sudo dnf install yarnpkg
+# sudo npm install -g @mermaid-js/mermaid-cli
 
-cd report/assets
+cd report/assets/diagrams
 for i in *.mmd; do
-    mmdc -c mermaid_config.json \
+    mmdc -c ../mermaid_config.json \
          -i "$i" \
-         -o "${i%.*}.svg"
+         -o "..${i%.*}.svg"
 done
-cd ../..
+cd ../../..
+
+
 
 # then manually change `==` to `<=` for syntax_tree.svg with inspect element
 # becuase otherwise renders as `&lt;`
